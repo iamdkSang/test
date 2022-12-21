8e2f86d43491dd0c2ee20a9cb04b7eb7ea845b31
Sang Khanh Do <111406740+iamdkSang@users.noreply.github.com>
2022-12-21 10:07:03 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np 
 
-arr = [1,2,3]
+arr_1 = [1,2,3]
+arr_2 = [4,5,6]
 
-np_arr = np.array(arr)
+np_arr_1 = np.array(arr_1)
+np_arr_2 = np.array(arr_2)
 
-print(np_arr)
+print(np_arr_1 + np_arr_2)
